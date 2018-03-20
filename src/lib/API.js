058d85fd8d27a6963b8606811addf3bbc4a05854
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-20 15:45:20 -0600

    added url ternary

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:3000/api/v1/figures";
+const API_URL = window.location.hostname == "localhost" ?
+  "http://localhost:3000/api/v1/figures" :
+  "https://shfexpress.herokuapp.com/api/v1/figures";
 
 export default {
   async getAll() {
