44f9229e4206912b1871723e64329b2495dce65f
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-21 14:08:01 -0600

    updated nav bar links

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">SHF 2.0</a>
+        <a className="navbar-brand" href="#">S.H.Figuarts 2.0</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
@@ -15,10 +15,7 @@ class Header extends Component {
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">Collection</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">About</a>
